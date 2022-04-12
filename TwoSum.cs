e6d462c96cac8720c668c1f0099890ead3f10d7f
R1nge <r1ngegamedev@gmail.com>
2022-04-12 20:05:20 +0300

    Added Two Sum Solution

---
+++ TwoSum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] indexies = new int[2];
+        for(int i = 0; i < nums.Length; i++){
+            for(int j = i + 1; j < nums.Length; j++){
+                if(nums[i] + nums[j] == target){
+                    indexies[0] = i;
+                    indexies[1] = j;
+                    break;
+                }
+            }
+        }
+        return indexies;
+    }
+}
