ac70eaed635a1cfd959106b4806bee9083897334
R1nge <r1ngegamedev@gmail.com>
2022-04-26 11:08:39 -0700

    Create SelfDividingNumber.cs

---
+++ SelfDividingNumber.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public IList<int> SelfDividingNumbers(int left, int right) {
+        char[] charArray = new char[9999];
+        List<int> list = new List<int>();
+        int num = 0;
+        while(left <= right)
+        {
+            charArray = left.ToString().ToCharArray();
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                num = (int)Char.GetNumericValue(charArray[i]);
+                if(num != 0)
+                {
+                    if(left % num == 0)
+                    {
+                        if(i == charArray.Length - 1)
+                        {
+                            list.Add(left);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            left++;
+        }
+        return list;
+    }
+}
